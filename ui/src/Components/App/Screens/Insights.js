2a9ac50ba6221e7465da61531afe22a050b6408e
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-05-30 12:48:30 -0700

    populated new pages so nav will show up

---
+++ ui/src/Components/App/Screens/Insights.js
@@ -0,0 +1,11 @@
+export const Insights = () => {
+    return (
+        <>
+            <section>
+                <div className="header-row">
+                    <h1>Insights Page</h1>
+                </div>
+            </section>
+        </>
+    );
+};
