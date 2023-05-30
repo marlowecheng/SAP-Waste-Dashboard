5dbb239c2887cc5df5cd02b6918f7ecf10e06dd1
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-05-30 16:00:31 -0700

    installed tailwind

--- ui/src/Components/App/Screens/Home.js
+++ ui/src/Components/App/Screens/Home.js
@@ -3,7 +3,7 @@ export const Home = () => {
         <>
             <section>
                 <div className="header-row">
-                    <h1>Home Page</h1>
+                    <h1 className="text-3xl font-bold underline">Home Page</h1>
                 </div>
             </section>
         </>
