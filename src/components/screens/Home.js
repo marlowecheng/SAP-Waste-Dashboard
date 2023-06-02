8699918f39ad27fbe7c7c4bc3c3433f748c06c5d
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-06-02 14:21:43 -0700

    made a home screen for routing later

---
+++ src/components/screens/Home.js
@@ -0,0 +1,11 @@
+export const Home = () => {
+    return (
+        <>
+            <section>
+                <div className="header-row">
+                    <h1 className="text-3xl font-bold underline">Home Page</h1>
+                </div>
+            </section>
+        </>
+    );
+};
